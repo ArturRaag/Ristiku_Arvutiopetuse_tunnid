19727e3cc8b1e09f4fc90d445c4f22f5ed7c3e06
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2021-03-17 05:48:16 +0200

    Create 2. tunni esimene harjutus.py

---
+++ Teine_nädal/2. tunni esimene harjutus.py
@@ -0,0 +1,10 @@
+
+
+nimi=input("Tere! Mis on sinu nimi? ")
+vanus=input("Tere "+ nimi +"! Mis on sinu vanus? ")
+
+print("Tere "+nimi+"! Sa oled "+vanus+" aastat vana.")
+
+print("\n")
+
+print("Tere "+nimi+"! Kas teadsid, et kümne aasta pärast oled "+ str(int(vanus)+10) +" aastat vana!")
