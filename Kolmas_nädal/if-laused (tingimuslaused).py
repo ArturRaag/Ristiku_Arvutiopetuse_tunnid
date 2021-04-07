6277c593514ad8c82dea691b357aef0d6b35a6a9
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2021-04-07 07:46:52 +0300

    Create if-laused (tingimuslaused).py

---
+++ Kolmas_nädal/if-laused (tingimuslaused).py
@@ -0,0 +1,9 @@
+parool="password"
+sisestatud_pw = input("Sisesta parool: ")
+
+if sisestatud_pw == parool:
+    print("Õige parool!")
+# elif sisestatud_pw != parool:
+#     print("Parool on vale.")
+else:
+    print("Parool on vale.")
