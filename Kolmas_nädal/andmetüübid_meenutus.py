3369a7417bba8dc6844020fe70fb30f138a42060
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2021-04-07 07:48:27 +0300

    Create andmetüübid_meenutus.py

---
+++ Kolmas_nädal/andmetüübid_meenutus.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 08:45:32 2021
+
+@author: artur
+"""
+
+arv1 = float(input("Sisesta esimene arv: "))
+arv2 = input("Sisesta teine arv: ")
+
+# print("Esimese ja teise arvu summa on: " + str((arv1+float(arv2))))
+print("Esimese ja teise arvu summa on: ", (arv1+float(arv2)))
+
+# str(), int(), float()
+
+
